5399f9a1572d614961995c33eb7da4dfbfcbc126
Logan Johnson <45829935+herariom@users.noreply.github.com>
2020-11-08 20:17:05 -0500

    Added login credentials for all S3 objects

--- s3_file.py
+++ s3_file.py
@@ -1,9 +1,12 @@
 import boto3
+import os
 
 # Upload file to AWS S3 Bucket
 def upload_file(file_name, bucket):
     object_name = file_name
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client('s3',
+                      aws_access_key_id=os.environ['S3_ACCESS_KEY'],
+                      aws_secret_access_key=os.environ['S3_SECRET_KEY'])
     response = s3_client.upload_file(file_name, bucket, object_name)
 
     return response
@@ -11,7 +14,9 @@ def upload_file(file_name, bucket):
 # Download file from AWS S3 Bucket
 def download_file(file_name, bucket):
 
-    s3 = boto3.resource('s3')
+    s3 = boto3.resource('s3',
+                      aws_access_key_id=os.environ['S3_ACCESS_KEY'],
+                      aws_secret_access_key=os.environ['S3_SECRET_KEY'])
     output = f"downloads/{file_name}"
     s3.Bucket(bucket).download_file(file_name, output)
 
